64f282e6b2f0a5cc0ba607004c53dbd33270505d
malshi <malshisathsarani1@gmail.com>
2025-03-08 14:08:55 +0530

    first commit

---
+++ resources/views/layouts/app-layout.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Index - HeroBiz Bootstrap Template</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+
+  <x-libraries.styles />
+
+
+</head>
+
+<body class="index-page">
+
+
+  <x-components.navigation />
+
+  <main class="main">
+
+  {{ $page }}
+
+  </main>
+
+
+  <x-components.footer />
+
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
+
+  <x-libraries.scripts />
+
+
+</body>
+
+</html>
